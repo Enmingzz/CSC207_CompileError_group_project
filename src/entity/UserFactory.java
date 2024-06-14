b96993470498ef2d5c866a3ed4e93528519ab264
Haoliang Zhao <haoliang.zhao@mail.utoronto.ca>
2024-06-14 16:39:22 -0400

    Data access finished

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -4,8 +4,5 @@ import java.util.ArrayList;
 
 public interface UserFactory {
     public User createUser(String name, String Password, String email, ArrayList<Product> ProductList, ShoppingCart userCart, float userRating);
-
-    public static void main(String[] args) {
-        System.out.println("hello world!");
     }
 }
