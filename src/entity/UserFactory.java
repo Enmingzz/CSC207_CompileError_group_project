cc55caedb55cf4bb4ddedb47530b9c64b1077163
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-06-14 15:44:09 -0400

    Test features

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -6,6 +6,6 @@ public interface UserFactory {
     public User createUser(String name, String Password, String email, ArrayList<Product> ProductList, ShoppingCart userCart, float userRating);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("nibuhao");
     }
 }
