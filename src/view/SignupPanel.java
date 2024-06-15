d512201b674003f1351c49755494d8dd187a5dad
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-06-15 13:48:37 -0400

    Add more features in Interface adapters.

---
+++ src/view/SignupPanel.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.awt.*;
+
+public class SignupPanel {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        Panel panel = new Panel();
+        panel.add(new TextField("hello world!"));
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setBounds(100,100,1000,1000);
+    }
+}
--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -1,4 +1,12 @@
 package view;
 
+import java.awt.*;
+
 public class SignupView {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setTitle("Signup");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
 }
