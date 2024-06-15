36f37896946559fb891f13caf7c751a806afe17d
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-06-15 14:01:18 -0400

    Try to add some Javadocs

--- src/entity/CommonAnswerFactory.java
+++ src/entity/CommonAnswerFactory.java
@@ -1,5 +1,9 @@
 package entity;
 
+/**
+ * Implementation of the interface AnswerFactory and return an instance of CommonAnswer with upcasting to Answer
+ * @author CompileError group
+ */
 public class CommonAnswerFactory {
     Answer createCommonAnswer(String description, CommonUser commonUser){
         return new CommonAnswer(description, commonUser);
