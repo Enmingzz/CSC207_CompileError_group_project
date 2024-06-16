00f7c52810164c3345c4314de48ee4876262d558
Haoliang Zhao <haoliang.zhao@mail.utoronto.ca>
2024-06-16 00:05:39 -0400

    DatabaseUserSignupSaveDataAccessObject has finished. Work on DatabaseQuestionLoadDataAccessObject and DatabaseUserLoginLoadDataAccessObject. NOTE: Some attributes in some Entities should be rearranged, due to the conflict when retrieving from database.

--- src/data_access/QuestionLoadDataAccessInterface.java
+++ src/data_access/QuestionLoadDataAccessInterface.java
@@ -1,4 +1,9 @@
 package data_access;
 
+import entity.Question;
+
+import java.sql.SQLException;
+
 public interface QuestionLoadDataAccessInterface {
+    public Question loadQuestion(String question) throws SQLException;
 }
--- src/data_access/UserLoginDataAccessInterface.java
+++ src/data_access/UserLoginDataAccessInterface.java
@@ -1,4 +1,10 @@
 package data_access;
 
+import entity.User;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface UserLoginDataAccessInterface {
+    User getUser(String userEmail) throws SQLException, IOException;
 }
