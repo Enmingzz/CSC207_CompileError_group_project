dece5786ad3340025d3473a57bfaa4933b454015
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-06-17 15:45:22 -0400

    rename a file

--- src/entity/CommonScheduleFactory.java
+++ src/entity/CommonScheduleFactory.java
@@ -3,7 +3,7 @@ package entity;
 import java.sql.Time;
 import java.util.ArrayList;
 
-public class CommonScheduleFactor implements ScheduleFactory {
+public class CommonScheduleFactory implements ScheduleFactory {
     public Schedule createSchedule(Time buyerTime, ArrayList<Time> sellerTime) {
         return new CommonSchedule(buyerTime, sellerTime);
     }
