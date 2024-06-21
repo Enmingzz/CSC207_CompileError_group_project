e042972d3ecf26df6b381e5a462c71074356c518
zhr98971 <zhr.98971@outlook.com>
2024-06-21 17:59:00 -0400

    partial implementation of presenter layer of DeleteProductFromCart and others

--- src/data_access/interfaces/ShoppingCartReadDataAccessInterface.java
+++ src/data_access/interfaces/ShoppingCartReadDataAccessInterface.java
@@ -1,10 +1,9 @@
 package data_access.interfaces;
 
 import entity.shopping_cart.ShoppingCart;
-import entity.user.User;
 
 import java.sql.SQLException;
 
 public interface ShoppingCartReadDataAccessInterface {
-    ShoppingCart getShoppingCart(User user) throws SQLException;
+    ShoppingCart getShoppingCart(String userId) throws SQLException;
 }
