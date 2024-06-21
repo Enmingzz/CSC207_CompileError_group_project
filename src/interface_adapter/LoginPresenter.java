9522004c41b0cc9acdfd85158b29335690bd60c2
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-06-21 11:44:20 -0400

    write Loginusecase

--- src/interface_adapter/LoginPresenter.java
+++ src/interface_adapter/LoginPresenter.java
@@ -1,4 +1,26 @@
 package interface_adapter;
 
-public class LoginPresenter {
+import use_case.LoginOutputBoundary;
+
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final MainPageViewModel mainPageViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel, MainPageViewModel mainPageViewModel) {
+        this.loginViewModel = loginViewModel;
+        this.mainPageViewModel = mainPageViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessfulView() {
+
+    }
+
+    @Override
+    public void prepareFailedView() {
+
+    }
 }
--- src/use_case/LoginInteractor.java
+++ src/use_case/LoginInteractor.java
@@ -1,4 +1,34 @@
 package use_case;
 
+import data_access.interfaces.UserReadDataAccessInterface;
+import entity.user.User;
+import entity.user.UserFactory;
+
+import java.sql.SQLException;
+
 public class LoginInteractor implements LoginInputBoundary{
+
+    final UserReadDataAccessInterface userReadDataAccessObject;
+    final LoginOutputBoundary loginPresenter;
+    final UserFactory userFactory;
+
+    public LoginInteractor(UserReadDataAccessInterface userReadDataAccessObject, LoginOutputBoundary loginPresenter, UserFactory userFactory) {
+        this.userReadDataAccessObject = userReadDataAccessObject;
+        this.loginPresenter = loginPresenter;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(LoginInputData loginInputData) throws SQLException {
+        User user = userReadDataAccessObject.getUser(loginInputData.getUsername());
+        if (user == null){
+            loginPresenter.prepareFailedView();
+        }
+        else if(user.getPassword().equals(loginInputData.getPassword())) {
+            loginPresenter.prepareSuccessfulView();
+        }
+        else if(!user.getPassword().equals(loginInputData.getPassword())){
+            loginPresenter.prepareFailedView();
+        }
+    }
 }
