e5f8440cbd6a3c9a340e50d2195c1430a2353cc7
freyazhixiaofu <149891141+freyazhixiaofu@users.noreply.github.com>
2024-06-22 14:11:21 -0400

    viewproduct adapter

---
+++ src/interface_adapter/PublishQuestionController.java
@@ -0,0 +1,22 @@
+package interface_adapter;
+
+import entity.comment.Question;
+import entity.product.Product;
+import use_case.PublishQuestionInputBoundary;
+import use_case.PublishQuestionInputData;
+
+import java.sql.SQLException;
+
+public class PublishQuestionController {
+    final PublishQuestionInputBoundary publishQuestionInteractor;
+
+    public PublishQuestionController(PublishQuestionInputBoundary publishQuestionInputBoundary){
+        this.publishQuestionInteractor = publishQuestionInputBoundary;
+    }
+
+    public void execute(Question question, Product product) throws SQLException {
+        PublishQuestionInputData publishQuestionInputData = new PublishQuestionInputData(question, product);
+        publishQuestionInteractor.execute(publishQuestionInputData);
+    }
+
+}
