dffc7b6064e069b49f194dca77e8b31c13c57e0f
zhr98971 <zhr.98971@outlook.com>
2024-06-23 22:16:39 -0400

    added shoppingCart view panels

---
+++ src/view/ShoppingCartRatingPanel.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class ShoppingCartRatingPanel extends JPanel {
+    ShoppingCartRatingPanel(JButton viewProductButton, JButton ratingButton){
+        this.add(viewProductButton);
+        this.add(ratingButton);
+    }
+}
