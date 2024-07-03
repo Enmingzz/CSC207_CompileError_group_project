1ccc9def7fd0ac2c42a17542d62c0348ac55aa96
fafafkekkefeng <kw.feng@mail.utoronto.ca>
2024-07-03 11:03:35 -0400

    product_search use case and interface adapter

--- src/interface_adapter/search_product/SearchProductByTagPresenter.java
+++ src/interface_adapter/search_product/SearchProductByTagPresenter.java
@@ -1,4 +1,18 @@
 package interface_adapter.search_product;
 
-public class SearchProductByTagPresenter {
+import use_case.product_search.SearchProductByTagOutputBoundary;
+import use_case.product_search.SearchProductByTagOutputData;
+
+public class SearchProductByTagPresenter implements SearchProductByTagOutputBoundary {
+    private final SearchProductByTagViewModel viewModel;
+
+    public SearchProductByTagPresenter(SearchProductByTagViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessfulView(SearchProductByTagOutputData searchProductByTagOutputData) {
+        viewModel.setProducts(searchProductByTagOutputData.getProducts());
+    }
+
 }
