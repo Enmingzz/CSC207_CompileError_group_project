a483395cf15747afdba44ce9247ff59ea203d10f
fafafkekkefeng <kw.feng@mail.utoronto.ca>
2024-07-03 16:06:40 -0400

    add files and delete mistakes

--- src/interface_adapter/ViewManagerModel.java
+++ src/interface_adapter/ViewManagerModel.java
@@ -6,9 +6,6 @@ import java.beans.PropertyChangeSupport;
 public class ViewManagerModel {
 
     private String activeViewName;
-    private String productId;
-    private String buyerName;
-    private String sellerName;
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
@@ -24,29 +21,6 @@ public class ViewManagerModel {
         support.firePropertyChange("view", null, this.activeViewName);
     }
 
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getBuyerName() {
-        return buyerName;
-    }
-
-    public void setBuyerName(String buyerName) {
-        this.buyerName = buyerName;
-    }
-
-    public String getSellerName() {
-        return sellerName;
-    }
-
-    public void setSellerName(String sellerName) {
-        this.sellerName = sellerName;
-    }
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
