ccce5cd9dc0c674a4a448c8b71a31b3e8eb9ea6b
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-07-08 20:23:45 -0400

    create Test file in data object

---
+++ test/data_access/factories/objects/user/DatabaseUserUpdatePasswordDataAccessObjectFactoryTest.java
@@ -0,0 +1,22 @@
+package data_access.factories.objects.user;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseUserUpdatePasswordDataAccessObjectFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+    }
+}
