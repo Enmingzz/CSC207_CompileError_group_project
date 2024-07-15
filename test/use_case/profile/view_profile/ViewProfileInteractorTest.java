751ade71f17c9dcbf415c01ce55e86ac5dff944a
Haoliang Zhao <haoliang.zhao@mail.utoronto.ca>
2024-07-15 13:51:18 -0400

    * Keep implementing Profile. * Starting to test Profile use case interactor.

--- test/use_case/profile/view_profile/ViewProfileInteractorTest.java
+++ test/use_case/profile/view_profile/ViewProfileInteractorTest.java
@@ -1,5 +1,8 @@
 package use_case.profile.view_profile;
 
+import entity.user.CommonUser;
+import entity.user.CommonUserFactory;
+import entity.user.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,9 +10,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ViewProfileInteractorTest {
+    private ViewProfileOutputBoundary viewProfileOutputBoundary;
+
+    private ViewProfileInputBoundary viewProfileInteractor;
+    private ViewProfileInputData viewProfileInputData;
+
+    private User user;
+
 
     @BeforeEach
     void setUp() {
+        user = new CommonUser("hanrui", "123456", "hanrui@mail", 0, "123456");
+        viewProfileInputData = new ViewProfileInputData(user);
     }
 
     @AfterEach
@@ -18,5 +30,15 @@ class ViewProfileInteractorTest {
 
     @Test
     void execute() {
+        viewProfileOutputBoundary = new ViewProfileOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(ViewProfileOutputData viewProfileOutputData) {
+                assertEquals(user, viewProfileOutputData.getUser());
+            }
+        };
+
+        viewProfileInteractor = new ViewProfileInteractor(viewProfileOutputBoundary);
+
+        viewProfileInteractor.execute(viewProfileInputData);
     }
 }
