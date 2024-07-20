8838b20d66f30ba708fa81475b881dbc64428ac1
freyazhixiaofu <149891141+freyazhixiaofu@users.noreply.github.com>
2024-07-19 23:48:46 -0400

    delete mytest, which was accidentally submitted

--- src/MyTest.java
+++
@@ -1,8 +0,0 @@
-public class MyTest {
-    public static void main(String[] args) {
-        String test = "[1, 2, 3]";
-        char[] password = {'1', '2', '3'};
-        System.out.println(test.equals(String.valueOf(password)));
-        System.out.println(String.valueOf(password));
-    }
-}
