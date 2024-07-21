3cda5df0e20643d0100e4ffe8f62ab6be4afa27e
enmingzhangzz <enming.zhang@student.tdsb.on.ca>
2024-07-21 19:28:40 -0400

    fix bugs in user profile use case interactor test

--- test/use_case/profile/view_profile/ViewUserProfileInteractorTest.java
+++ test/use_case/profile/view_profile/ViewUserProfileInteractorTest.java
@@ -1,12 +1,17 @@
 package use_case.profile.view_profile;
 
 import data_access.in_memory.product.InMemoryProductReadByUserDataAccessObject;
+import data_access.in_memory.user.InMemoryUserDataReadAccessObject;
+import data_access.interfaces.product.ProductReadByUserDataAccessInterface;
+import data_access.interfaces.user.UserReadDataAccessInterface;
 import entity.product.CommonProduct;
 import entity.product.Product;
 import entity.schedule.CommonSchedule;
 import entity.schedule.Schedule;
 import entity.user.CommonUser;
+import entity.user.CommonUserFactory;
 import entity.user.User;
+import entity.user.UserFactory;
 import interface_adapter.profile.view_profile.ViewUserProfilePresenter;
 import interface_adapter.profile.view_profile.ViewUserProfileViewModel;
 import org.junit.jupiter.api.AfterEach;
@@ -17,6 +22,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -37,22 +43,29 @@ class ViewUserProfileInteractorTest {
     private LocalDateTime time;
     private ArrayList<LocalDateTime> localDateTimeList;
     private Product commonProduct;
-    private InMemoryProductReadByUserDataAccessObject productReadByUserDataAccessObject;
+    private ProductReadByUserDataAccessInterface productReadByUserDataAccessObject;
+    private UserReadDataAccessInterface userReadDataAccessObject;
 
     @BeforeEach
-    void setUp() throws IOException {
+    void setUp() throws IOException, SQLException {
         products = new ArrayList<>();
+        tags = new ArrayList<>();
         tags.add("tag1");
-        image = ImageIO.read(new File("/src/pic/testpic1"));
+        image = null;
         time = LocalDateTime.now();
         localDateTimeList = new ArrayList<>();
         localDateTimeList.add(time);
         commonSchedule = new CommonSchedule(time, localDateTimeList);
         commonProduct = new CommonProduct(image, "test product", "pro", 10, 5, 0,
                 "hanrui@mail", "123456", "hanrui123456", tags, "123456", commonSchedule);
-        studentNumber = "12345";
+        studentNumber = "123456";
         products.add(commonProduct);
         productReadByUserDataAccessObject = new InMemoryProductReadByUserDataAccessObject(products);
+        User commonUser = new CommonUser("enming", "123", "enming.zhang@mail", 0, "123456");
+        UserFactory userFactory = new CommonUserFactory();
+        ArrayList<User> users = new ArrayList<>();
+        users.add(commonUser);
+        userReadDataAccessObject = new InMemoryUserDataReadAccessObject(users, userFactory);
         buyer = new CommonUser("hanrui", "1234", "hanrui@mail", 1,"12122");
         inputData = new ViewUserProfileInputData(studentNumber, buyer);
         ViewUserProfileOutputBoundary viewUserProfilePresenter = new ViewUserProfileOutputBoundary() {
@@ -60,10 +73,24 @@ class ViewUserProfileInteractorTest {
             public void prepareSuccessfulView(ViewUserProfileOutputData viewUserProfileOutputData) {
                 assertEquals(studentNumber, viewUserProfileOutputData.getSeller().getStudentNumber());
                 assertEquals(buyer, viewUserProfileOutputData.getBuyer());
-                assertEquals(products, viewUserProfileOutputData.getProductList());
+                assertEquals(products.get(0).getDescription(),
+                        viewUserProfileOutputData.getProductList().get(0).getDescription());
+                assertEquals(products.get(0).getProductID(),
+                        viewUserProfileOutputData.getProductList().get(0).getProductID());
+                assertEquals(products.get(0).getPrice(),
+                        viewUserProfileOutputData.getProductList().get(0).getPrice());
+                assertEquals(products.get(0).getImage(),
+                        viewUserProfileOutputData.getProductList().get(0).getImage());
+                assertEquals(products.get(0).geteTransferEmail(),
+                        viewUserProfileOutputData.getProductList().get(0).geteTransferEmail());
+                assertEquals(products.get(0).getSellerStudentNumber(),
+                        viewUserProfileOutputData.getProductList().get(0).getSellerStudentNumber());
             }
         };
 
+        interactor = new ViewUserProfileInteractor(viewUserProfilePresenter,
+                userReadDataAccessObject, productReadByUserDataAccessObject);
+        interactor.execute(inputData);
     }
 
     @AfterEach
