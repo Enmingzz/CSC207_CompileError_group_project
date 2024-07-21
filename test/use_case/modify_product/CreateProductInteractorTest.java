58c65737b069893a70fc35e777962e3c08645484
celinac23 <Celina.y.125@gmail.com>
2024-07-21 16:06:38 -0400

    changes to unit tests for changeProduct and createProduct

--- test/use_case/modify_product/CreateProductInteractorTest.java
+++ test/use_case/modify_product/CreateProductInteractorTest.java
@@ -7,6 +7,7 @@ import data_access.interfaces.product.ProductCreateDataAccessInterface;
 import data_access.interfaces.product.ProductReadAllDataAccessInterface;
 import data_access.interfaces.product.ProductReadByIdDataAccessInterface;
 import entity.product.CommonProductFactory;
+import entity.product.Product;
 import entity.product.ProductFactory;
 import entity.schedule.CommonScheduleFactory;
 import entity.schedule.Schedule;
@@ -39,6 +40,8 @@ class CreateProductInteractorTest {
     private float priceFloat;
     private ProductFactory productFactory;
 
+    private ArrayList<Product> emptyList = new ArrayList<>();
+
     @BeforeEach
     void setUp() throws IOException {
         String name = "Calico";
@@ -60,10 +63,7 @@ class CreateProductInteractorTest {
     void prepareSuccessfulViewTest1() throws IOException, SQLException {
         /** This is a test where the creation of the product is successful */
         ProductReadByIdDataAccessInterface inMemoryProductReadByIdDataAccessObject =
-                new InMemoryProductReadByIdDataAccessObject();
-
-        ProductReadAllDataAccessInterface inMemoryProductReadAllDataAccessObject =
-                new InMemoryProductReadAllDataAccessObject();
+                new InMemoryProductReadByIdDataAccessObject(emptyList);
 
         image = ImageIO.read(new File("src/pic/testpic1.png"));
         description = "It was worn once";
@@ -89,9 +89,30 @@ class CreateProductInteractorTest {
                 assertEquals(user, createProductOutputData.getUser());
 
                 //ensures that it is saved into the database
-                assertEquals(inMemoryProductReadAllDataAccessObject.getAllProducts(), createProductOutputData.getProduct());
-                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()),
-                        createProductOutputData.getProduct());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getPrice(),
+                        createProductOutputData.getProduct().getPrice());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getImage(),
+                        createProductOutputData.getProduct().getImage());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getDescription(),
+                        createProductOutputData.getProduct().getDescription());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getTitle(),
+                        createProductOutputData.getProduct().getTitle());
+
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getAddress(),
+                        createProductOutputData.getProduct().getAddress());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getSchedule().getBuyerTime(),
+                        createProductOutputData.getProduct().getSchedule().getBuyerTime());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getSchedule().getSellerTime(),
+                        createProductOutputData.getProduct().getSchedule().getSellerTime());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getState(),
+                        createProductOutputData.getProduct().getState());
+
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getSellerStudentNumber(),
+                        createProductOutputData.getProduct().getSellerStudentNumber());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).geteTransferEmail(),
+                        createProductOutputData.getProduct().geteTransferEmail());
+                assertEquals(inMemoryProductReadByIdDataAccessObject.getProductById(createProductOutputData.getProduct().getProductID()).getListTags(),
+                        createProductOutputData.getProduct().getListTags());
             }
             @Override
             public void prepareFailedView(String error) {
@@ -100,7 +121,7 @@ class CreateProductInteractorTest {
         };
 
         productFactory = new CommonProductFactory();
-        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject(emptyList);
         CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
                 createProductOutputBoundary, productFactory);
 
@@ -109,7 +130,7 @@ class CreateProductInteractorTest {
     }
 
     @Test
-    void prepareFailedView1() throws IOException, SQLException {
+    void prepareFailedViewImage() throws IOException, SQLException {
         /** This is a test where the image fails to upload */
 
         image = null;
@@ -142,12 +163,45 @@ class CreateProductInteractorTest {
     }
 
     @Test
-    void prepareFailedView2() throws IOException, SQLException {
-        /** This is a test where the image fails to upload */
+    void prepareFailedViewDescription() throws IOException, SQLException {
+        /** This is a test where the description field is empty, this is not allowed. */
 
         image = ImageIO.read(new File("src/pic/testpic1.png"));
         description = "";
-        price = "9";
+        price = "-10";
+        priceFloat = Float.parseFloat(price);
+        title = "Red Dress";
+        eTransferEmail = "calico.cat@mail.utoronto.ca";
+        address = "123College";
+        listTags = new ArrayList<>();
+        listTags.add("clothes");
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must enter a valid description for the product.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
+    @Test
+    void prepareFailedViewPrice1() throws IOException, SQLException {
+        /** This is a test where the price they entered is negative. */
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "-10";
         priceFloat = Float.parseFloat(price);
         title = "Red Dress";
         eTransferEmail = "calico.cat@mail.utoronto.ca";
@@ -173,4 +227,168 @@ class CreateProductInteractorTest {
         CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
         createProductInteractor.execute(inputData);
     }
+
+    @Test
+    void prepareFailedViewPrice2() throws IOException, SQLException {
+        /** This is a test where the price they entered is not a float*/
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "asdsd";
+        title = "Red Dress";
+        eTransferEmail = "calico.cat@mail.utoronto.ca";
+        address = "123College";
+        listTags = new ArrayList<>();
+        listTags.add("clothes");
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must indicate a valid price of the product.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
+    @Test
+    void prepareFailedViewTitle() throws IOException, SQLException {
+        /** This is a test where the title field is empty.*/
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "9";
+        priceFloat = Float.parseFloat(price);
+        title = "";
+        eTransferEmail = "calico.cat@mail.utoronto.ca";
+        address = "123College";
+        listTags = new ArrayList<>();
+        listTags.add("clothes");
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must provide a valid title for the product.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
+    @Test
+    void prepareFailedViewETransferEmail() throws IOException, SQLException {
+        /** This is a test where the etranser email field is empty.*/
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "9";
+        priceFloat = Float.parseFloat(price);
+        title = "Red Dress";
+        eTransferEmail = "";
+        address = "123College";
+        listTags = new ArrayList<>();
+        listTags.add("clothes");
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must provide a valid eTransfer email for the product.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
+    @Test
+    void prepareFailedViewAddress() throws IOException, SQLException {
+        /** This is a test where the address field is empty.*/
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "9";
+        priceFloat = Float.parseFloat(price);
+        title = "Red Dress";
+        eTransferEmail = "calico.cat@mail.utoronto.ca";
+        address = "";
+        listTags = new ArrayList<>();
+        listTags.add("clothes");
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must provide a valid address for the pickup location.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
+    @Test
+    void prepareFailedViewTags() throws IOException, SQLException {
+        /** This is a test where the address field is empty.*/
+
+        image = ImageIO.read(new File("src/pic/testpic1.png"));
+        description = "Brand new.";
+        price = "9";
+        priceFloat = Float.parseFloat(price);
+        title = "Red Dress";
+        eTransferEmail = "calico.cat@mail.utoronto.ca";
+        address = "123College";
+        listTags = new ArrayList<>();
+
+        CreateProductOutputBoundary createProductOutputBoundary = new CreateProductOutputBoundary() {
+            @Override
+            public void prepareSuccessfulView(CreateProductOutputData createProductOutputData) throws SQLException, IOException {
+                assert(false);
+            }
+            @Override
+            public void prepareFailedView(String error) {
+                assertEquals(error, "You must select at least one tag for the product.");
+            }
+        };
+
+        ProductCreateDataAccessInterface inMemoryProductCreateDataAccessObject = new InMemoryProductCreateDataAccessObject();
+        CreateProductInteractor createProductInteractor = new CreateProductInteractor(inMemoryProductCreateDataAccessObject,
+                createProductOutputBoundary, productFactory);
+
+        CreateProductInputData inputData = new CreateProductInputData(user, image, description, price, title, eTransferEmail, address, listTags);
+        createProductInteractor.execute(inputData);
+    }
+
 }
