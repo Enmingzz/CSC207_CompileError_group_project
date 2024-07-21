e6187c56a6ac66b313b203f1f023f1c54d93fb3e
celinac23 <Celina.y.125@gmail.com>
2024-07-21 19:43:31 -0400

    Rating javadoc for Interface Adapter

--- src/interface_adapter/rating/RateProductPresenter.java
+++ src/interface_adapter/rating/RateProductPresenter.java
@@ -4,25 +4,38 @@ import entity.product.Product;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.shopping_cart.ShoppingCartState;
 import interface_adapter.shopping_cart.ShoppingCartViewModel;
-import use_case.rate_product.RateProductInputData;
 import use_case.rate_product.RateProductOutputBoundary;
 import use_case.rate_product.RateProductOutputData;
-import view.ViewManager;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
+/**
+ * Presenter class for handling the output of the rate product use case.
+ * It updates the view models and manages the state based on the output data.
+ */
 public class RateProductPresenter implements RateProductOutputBoundary {
     private final RateProductViewModel rateProductViewModel;
     private final ViewManagerModel viewManagerModel;
     private final ShoppingCartViewModel shoppingCartViewModel;
 
+    /**
+     * Constructs a RateProductPresenter with the specified view models.
+     *
+     * @param rateProductViewModel   The view model for rating products.
+     * @param viewManagerModel       The view manager model.
+     * @param shoppingCartViewModel  The view model for the shopping cart.
+     */
     public RateProductPresenter(RateProductViewModel rateProductViewModel, ViewManagerModel viewManagerModel, ShoppingCartViewModel shoppingCartViewModel) {
         this.rateProductViewModel = rateProductViewModel;
         this.viewManagerModel = viewManagerModel;
         this.shoppingCartViewModel = shoppingCartViewModel;
     }
 
+    /**
+     * Prepares the successful view after a product is rated.
+     *
+     * @param rateProductOutputData The output data after rating a product.
+     */
     @Override
     public void prepareSuccessfulView(RateProductOutputData rateProductOutputData) {
         ShoppingCartState shoppingCartState = shoppingCartViewModel.getState();
@@ -38,11 +51,16 @@ public class RateProductPresenter implements RateProductOutputBoundary {
         shoppingCartState.setTotalPrice(totalPrice);
         shoppingCartViewModel.setState(shoppingCartState);
         shoppingCartViewModel.firePropertyChanged();
-        //since we want to return to the shopping cart view, we need to call setActiveView
+        // Since we want to return to the shopping cart view, we need to call setActiveView
         viewManagerModel.setActiveView("shopping cart");
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Prepares the failed view if rating a product fails.
+     *
+     * @param error The error message indicating why the rating failed.
+     */
     @Override
     public void prepareFailedView(String error) {
         RateProductState rateProductState = rateProductViewModel.getState();
--- src/interface_adapter/rating/RateProductViewModel.java
+++ src/interface_adapter/rating/RateProductViewModel.java
@@ -1,11 +1,13 @@
 package interface_adapter.rating;
 
 import interface_adapter.ViewModel;
-import interface_adapter.modify_product.CreateProductState;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * ViewModel for the RateProduct view. Contains state and labels for the view.
+ */
 public class RateProductViewModel extends ViewModel {
     public final String TITLE_LABEL = "Rate Product";
     public final String PRODUCT_IMAGE = "Product image";
@@ -17,24 +19,45 @@ public class RateProductViewModel extends ViewModel {
 
     private RateProductState state = new RateProductState();
 
+    /**
+     * Constructs a RateProductViewModel with a default view name.
+     */
     public RateProductViewModel() {
         super("view rate product");
     }
 
+    /**
+     * Gets the current state of the RateProductViewModel.
+     *
+     * @return The current RateProductState.
+     */
     public RateProductState getState() {
         return state;
     }
 
+    /**
+     * Sets the state of the RateProductViewModel.
+     *
+     * @param state The new RateProductState.
+     */
     public void setState(RateProductState state) {
         this.state = state;
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    /**
+     * Notifies all listeners that the state property has changed.
+     */
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
     }
 
+    /**
+     * Adds a PropertyChangeListener to the listener list.
+     *
+     * @param listener The PropertyChangeListener to be added.
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
